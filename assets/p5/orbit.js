2af1e9a954a96260e958bcdfba138754051e1608
Danny White <hi@dannywhite.site>
2021-08-06 17:52:25 +1000

    multiple images, corner stub

--- assets/p5/orbit.js
+++ assets/p5/orbit.js
@@ -2,7 +2,7 @@
 let parentEl, windowInnerWidth;
 
 // Prep image variables
-let ala, checkers, nativeCheckout;
+let ala, checkers, nativeCheckout, ticketNumber, raffleTicket, puttPutt, fridaKahlo, coffeeCup;
 
 // Global lerp amount variable (as it should be shared)
 let lerpAmt = 0.15;
@@ -33,7 +33,12 @@ const touch = matchMedia('(hover: none)').matches;
 function preload() {
     ala = loadImage("assets/images/ala-2-to-1-small-split.png");
     checkers = loadImage("assets/images/checkers.jpg");
-    nativeCheckout = loadImage("https://ik.imagekit.io/dw/work/native-checkout/log-in-modal.jpg")
+    ticketNumber = loadImage("https://dl.airtable.com/.attachments/864890b9250eabf9713fe922876a9508/ecaaf165/backpack-slip.jpg")
+    raffleTicket = loadImage("https://dl.airtable.com/.attachments/f74ea228cf924fd4aa48d183da689dad/d6318ea9/raffle-ticket-orange-c-47.jpg")
+    puttPutt = loadImage("https://dl.airtable.com/.attachments/fadbab2f3e2ef1e594299fce467eacea/83c6f882/maroochy-river-mini-golf.jpg")
+    postIt = loadImage("https://dl.airtable.com/.attachments/8743ca094a79fd48daa427df1f25d30a/1cacc068/post-it-mariela.jpg")
+    fridaKahlo = loadImage("https://dl.airtable.com/.attachments/fb830571d265fbe6e8941b6f8ec01020/5264b7b5/museo-frida-kahlo.jpg")
+    coffeeCup = loadImage("https://dl.airtable.com/.attachments/02b871b45dfdbae33fcc549910ef2929/5c634fb1/empanadas-cafe.jpg")
 }
 
 function setup() {
@@ -51,14 +56,49 @@ function setup() {
 
     // Prep outer-sphere inner-wall images
     gr = createGraphics(windowInnerWidth, windowHeight);
-    gr.image(checkers, 0, 0, 500, 500);
-    gr.image(checkers, canvas.width - windowInnerWidth - 500, 0, 500, 500);
-    
-    aspectRatio = nativeCheckout.height / nativeCheckout.width;
-    console.log(aspectRatio)
-    gr.image(nativeCheckout, canvas.width - windowInnerWidth - (canvas.width * 0.1), canvas.height - windowHeight - ((canvas.width * 0.1) * aspectRatio), canvas.width * 0.1, (canvas.width * 0.1) * aspectRatio);
-    // gr.image(nativeCheckout, (canvas.width - windowInnerWidth - 250) / 2, canvas.height - windowHeight - 250, 250);
+    // gr.image(checkers, 500, 0, 250, 250);
 
+    // aspectRatio = nativeCheckout.height / nativeCheckout.width;
+    // gr.image(nativeCheckout, canvas.width - windowInnerWidth - (canvas.width * 0.1), canvas.height - windowHeight - ((canvas.width * 0.1) * aspectRatio), canvas.width * 0.1, (canvas.width * 0.1) * aspectRatio);
+    // gr.image(nativeCheckout, 500, 0, 300, 300 * aspectRatio);
+
+    aspectRatio = ticketNumber.height / ticketNumber.width;
+    gr.image(ticketNumber, 0, canvas.height * 0.5, canvas.width * 0.033, (canvas.width * 0.033) * aspectRatio);
+
+    aspectRatio = raffleTicket.height / raffleTicket.width;
+    gr.image(raffleTicket, windowInnerWidth * 0.66, canvas.height * 0.5, canvas.width * 0.03, (canvas.width * 0.03) * aspectRatio);
+
+    aspectRatio = puttPutt.height / puttPutt.width;
+    gr.image(puttPutt, windowInnerWidth * 0.33, canvas.height * 0.6, canvas.width * 0.066, (canvas.width * 0.066) * aspectRatio);
+
+    aspectRatio = postIt.height / postIt.width;
+    gr.image(postIt, windowInnerWidth * 0.43, canvas.height * 0.3, canvas.width * 0.05, (canvas.width * 0.05) * aspectRatio);
+
+    aspectRatio = fridaKahlo.height / fridaKahlo.width;
+    gr.image(fridaKahlo, windowInnerWidth * 0.53, canvas.height * 0.4, canvas.width * 0.066, (canvas.width * 0.066) * aspectRatio);
+
+    aspectRatio = coffeeCup.height / coffeeCup.width;
+    gr.image(coffeeCup, windowInnerWidth * 0.63, canvas.height * 0.3, canvas.width * 0.066, (canvas.width * 0.066) * aspectRatio);
+
+
+    gr.textSize(windowInnerWidth * 0.025);
+    gr.textAlign(CENTER);
+    gr.fill('red');
+    gr.text('Welcome to the studio of Danny White', windowInnerWidth * 0.66, windowHeight *0.4);
+
+    gr.textAlign(RIGHT);
+    gr.text('📱 +1 (626) 657 7733 🇺🇸 • 🇦🇺 +61 490 520 628 📱', windowInnerWidth * 1, windowHeight * 0.25);
+    gr.text(`📧 ${basicEmailEncoder()} 📧`, windowInnerWidth * 0.89, windowHeight * 0.75);
+
+    gr.textAlign(LEFT);
+    gr.textSize(windowInnerWidth * 0.015);
+    gr.text('Fully vaxxed and ready to work', 0, windowHeight * 0.6);
+
+}
+
+function basicEmailEncoder() {
+    let content = ["org", "i@", "h", "nywhite", "dan"];
+    return `${content[2]}${content[1]}${content[4]}${content[3]}.${content[0]}`;
 }
 
 function draw() {
@@ -78,15 +118,17 @@ function draw() {
     let camY = map(camLerpY, 0, height, height * camStr, height * -camStr);
     camera(camX, camY, width, 0, 0, 0, 0, 1, 0);
 
-    // Set angle for ambient rotation
-    angle -= 0.05;
+    
 
     // Create the outer sphere
     push();
     scale(-1, 1);
     texture(gr);
+    // Set angle for ambient rotation
+    // angle -= 0.1;
+    angle -= 0.6;
     rotateY(angle * 0.02);
-    sphere(windowInnerWidth * 1)
+    sphere(windowInnerWidth * 1, 48, 48)
     pop();
 
     // Control rotation of the eye
@@ -105,10 +147,9 @@ function draw() {
         // Push this rotation so it isn't hardcoded
         // Pop after the screen has been touched
         push();
-        rotateX(angle * 0.02);
-        // console.log(testVar);
-        rotateY(angle * 0.05);
-        rotateZ(angle * 0.001);
+        // rotateX(angle * 0.02);
+        // rotateY(angle * 0.05);
+        // rotateZ(angle * 0.001);
 
         // Store mouseIsPressed information in a boolean for permanent rotation changes
         if (mouseIsPressed) {
--- index.md
+++ index.md
@@ -4,36 +4,16 @@ layout: index
 # No title here as this propagates to head
 ---
 
-# Oh hello there
+# Danny White
 
-This is Danny speaking.
+Digital product designer available for freelance and contract work.
 
-Here's a little bit about me, under the fold.
+Availability starting mid November 2021.
 
-It's all good stuff.
+Most recently I was a Staff Designer at Kickstarter, where I led the design for its iOS and Android applications. I also led design for Kickstarter's environmental work, including the [Environmental Resources Center]({% link _work/environmental-resources-center.md %}), and help design the project creation experience.
 
-Here's a little bit about me, under the fold.
+At ustwo, I helped create and foster projects with the likes of Facebook, Nike, Google, GE, AT&T, and the Office of the Mayor of New York City. The digital platform we began for the Office of the Mayor of New York City went on to receive an Adobe Government Creativity Award in 2018.
 
-It's all good stuff.
-
-Here's a little bit about me, under the fold.
-
-It's all good stuff.
-
-Here's a little bit about me, under the fold.
-
-It's all good stuff.
-
-Here's a little bit about me, under the fold.
-
-It's all good stuff.
-
-Here's a little bit about me, under the fold.
-
-It's all good stuff.
-
-Here's a little bit about me, under the fold.
-
-It's all good stuff.
+At Facebook I helped the Design Tools team bring Origami to life; contributing software design and creating the internal and [external learning experience](https://origami.design/tutorials){:target="_blank" rel="noopener noreferrer"} (tutorial videos, examples, and their interconnections) that live on today. I also worked on the Interfaces team at Facebook, where I created the company's first Origami component library and helped envision Facebook product UI changes.
 
 And then there might be links down here to other things I do on this site...
