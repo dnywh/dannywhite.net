12953a74e572c11acfada8965bc9f8bc80850270
Danny White <hi@dannywhite.org>
2021-11-01 16:55:09 +1100

    feat: sass

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,26 @@
+const htmlmin = require("html-minifier");
+
 module.exports = function (config) {
     // Copy folders into output directory
-    config.addPassthroughCopy("./src/css");
-    config.addPassthroughCopy("./src/img");
-    config.addPassthroughCopy("./src/js");
+    config.addWatchTarget("./src/sass/");
+    config.addPassthroughCopy("./src/img/");
+    config.addPassthroughCopy("./src/js/");
+
+    // Minify HTML and some other things
+    config.addTransform("htmlmin", function (content, outputPath) {
+        if (outputPath && outputPath.endsWith(".html")) {
+            let minified = htmlmin.minify(content, {
+                useShortDoctype: true,
+                removeComments: true,
+                collapseWhitespace: true,
+                preserveLineBreaks: false,
+                minifyJS: true,
+                minifyURLs: true
+            });
+            return minified;
+        }
+        return content;
+    });
 
     return {
         // Set which directories Eleventy reads from and writes to
--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# Page header
+# dannywhite.org
+
+My home on the internet, built on [Eleventy](https://www.11ty.dev).
+
+## Sass
+
+### Style
+
+Follows the Sass [7-1 Pattern](https://sass-guidelin.es/#the-7-1-pattern).
+
+### Architecture
+
+Based on [11ty Sass Skeleton](https://github.com/5t3ph/11ty-sass-skeleton) by @5t3ph.
+
+## Development Scripts
+
+**`npm start`**
+
+> Run 11ty with hot reload at localhost:8080, including reload based on Sass changes
+
+**`npm run build`**
+
+> Production build includes minified, autoprefixed CSS
+
+Use this as the "Publish command" if needed by hosting such as Netlify.
+
+## Credits
+
+- [11ty Sass Skeleton](https://github.com/5t3ph/11ty-sass-skeleton) by @5t3ph
+- [the-new-css-reset](https://github.com/elad2412/the-new-css-reset) by @elad2412
