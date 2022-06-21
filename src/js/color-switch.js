31208d2aaba86317054bb2a3a238878a2f4450d2
Danny White <hi@dannywhite.org>
2022-06-21 13:05:44 -0400

    fix: fully functioning switch and media queries

--- src/js/color-switch.js
+++ src/js/color-switch.js
@@ -1,12 +1,16 @@
 // Handles the color scheme toggle behavior
 // https://piccalil.li/tutorial/create-a-user-controlled-dark-or-light-mode/
+
+// Store the user's preference in local storage
 const STORAGE_KEY = 'user-color-scheme';
+// Extract the current CSS custom property value
 const COLOR_MODE_KEY = '--color-mode';
 
+// Get other HTML elements for later
 const modeToggleButton = document.querySelector('.js-mode-toggle');
-// const modeToggleText = document.querySelector('.js-mode-toggle-text');
 const modeStatusElement = document.querySelector('.js-mode-status');
 
+// Spit out either 'light' or 'dark' depending on the media query
 const getCSSCustomProp = propKey => {
     let response = getComputedStyle(document.documentElement).getPropertyValue(propKey);
 
@@ -17,26 +21,29 @@ const getCSSCustomProp = propKey => {
     return response;
 };
 
+// Apply the user's preference and updates button status
 const applySetting = passedSetting => {
     let currentSetting = passedSetting || localStorage.getItem(STORAGE_KEY);
 
     if (currentSetting) {
         document.documentElement.setAttribute('data-user-color-scheme', currentSetting);
-        setButtonLabelAndStatus(currentSetting);
+        setButtonStatus(currentSetting);
     } else {
-        setButtonLabelAndStatus(getCSSCustomProp(COLOR_MODE_KEY));
+        setButtonStatus(getCSSCustomProp(COLOR_MODE_KEY));
     }
 };
 
-const setButtonLabelAndStatus = currentSetting => {
-    // modeToggleText.innerText = `Enable ${currentSetting === 'dark' ? 'light' : 'dark'
-    //     } mode`;
+// Set button status (and text if I wanted that)
+const setButtonStatus = currentSetting => {
     modeStatusElement.innerText = `Color mode is now "${currentSetting}"`;
 };
 
+// Toggle the color mode
 const toggleSetting = () => {
+    // Try to load the current setting from local storage
     let currentSetting = localStorage.getItem(STORAGE_KEY);
 
+    // Switch the setting to the opposite
     switch (currentSetting) {
         case null:
             currentSetting = getCSSCustomProp(COLOR_MODE_KEY) === 'dark' ? 'light' : 'dark';
@@ -48,16 +55,19 @@ const toggleSetting = () => {
             currentSetting = 'light';
             break;
     }
-
+    // No setting found in local storage, set from CSS
     localStorage.setItem(STORAGE_KEY, currentSetting);
 
+    // Return new setting
     return currentSetting;
 };
 
+// Add an event to the button
 modeToggleButton.addEventListener('click', evt => {
     evt.preventDefault();
-
+    // Apply the new setting
     applySetting(toggleSetting());
 });
 
-applySetting();
+// Run this by default to make sure the user's preference is applied on page load
+// applySetting();
