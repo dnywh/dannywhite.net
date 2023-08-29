8bbdafc25d7156c785dd20191f02181dc503b584
Danny White <hi@dannywhite.org>
2023-08-29 15:57:37 +1000

    feat: better relative dates

--- src/_includes/js/relative-date-formatter.js
+++ src/_includes/js/relative-date-formatter.js
@@ -12,11 +12,12 @@ const short = new Intl.DateTimeFormat(
     navigator.language, { weekday: 'long' }
 );
 
-// Formatter for dates, e.g. "Thursday, 23 November 2023"
+// Formatter for dates, e.g. "November 2022"
+// Just to month and year
 const long = new Intl.DateTimeFormat(
     navigator.language, {
-    weekday: 'long',
-    day: 'numeric',
+    // weekday: 'long',
+    // day: 'numeric',
     month: 'long',
     year: 'numeric'
 });
@@ -26,13 +27,16 @@ const formatDate = (date) => {
     const now = new Date().setHours(0, 0, 0, 0);
     const then = date.setHours(0, 0, 0, 0);
     const days = (then - now) / 86400000;
+    // Format to relative date if today or yesterday
     if (days > -6) {
         if (days > -2) {
             return relative.format(days, 'day');
         }
+        // Otherwise the name of the date of the week
         return "on " + short.format(date);
     }
-    return "on " + long.format(date);
+    // Otherwise just the month and year
+    return long.format(date);
 };
 
 // Loop through all dates to convert
